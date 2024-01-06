60a1bcab211c7194079acf870b589367a59bcebf
Aruna Turay <tura_arun@yahoo.com>
2024-01-06 13:24:47 -0500

    moves all the elements into the js

---
+++ script.js
@@ -0,0 +1,6 @@
+const video = document.getElementById('video')
+const play = document.getElementById('play')
+const stop = document.getElementById('stop')
+const progress = document.getElementById('progress')
+const timeStamp = document.getElementById('timestamp')
+
