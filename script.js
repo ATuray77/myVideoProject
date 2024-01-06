3ce4d6bb124eec635f26ebfc0752ec36cc7159d5
Aruna Turay <tura_arun@yahoo.com>
2024-01-06 13:33:50 -0500

    adds event listeners for play and pause. Also adds play pause funacion

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const video = document.getElementById('video')
-const play = document.getElementById('play')
-const stop = document.getElementById('stop')
-const progress = document.getElementById('progress')
-const timeStamp = document.getElementById('timestamp')
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timeStamp = document.getElementById('timestamp');
 
+
+function playPause() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause;
+    }
+}
+
+
+video.addEventListener('click', playPause);
+play.addEventListener('click', playPause);
